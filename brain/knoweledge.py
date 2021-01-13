b5c39c080fd95dea59f46f6f3dcf0d3f673c1866
mario muuk <62722269+DrSuperMario@users.noreply.github.com>
2021-01-13 23:05:00 +0200

    updated CV

---
+++ brain/knoweledge.py
@@ -0,0 +1,25 @@
+import matplotlib.image as imgRead
+import pandas as pd
+
+
+pic = imgRead.imread('brain\ink.png')
+
+def tabel_of_know():
+
+    df = pd.DataFrame(index=None, columns=['Languages','Tools','Cloud','Enviroments', 'Other'])
+    df['Languages'] = ['JAVA','Python','SQL/HTML/CSS','Octave','Nan','Nan']
+    df['Tools'] = ['VSCode','Intellij','Android Studio','Postman','Octave','PowerShell']
+    df['Cloud'] = ['DigitalOcean','Heroku','TravisCI','Nan','Nan','Nan']
+    df['Enviroments'] = ['Windows', 'Ubuntu', 'ArchLinux','Nan','Nan','Nan']
+    df['Other'] = ['Machine Learning','Deep Learning','Data Science','Nan','Nan','Nan']
+    df = df.replace('Nan',' ')
+    return df.style.set_properties(**{'background-color': 'white',
+                           'color': 'black',
+                           'border-color': 'white',
+                           'font-size':'16px',
+                           'text-align':'left',
+                           'font-weight':'bold'}).hide_index()
+
+
+
+    
