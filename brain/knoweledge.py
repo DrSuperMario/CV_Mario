14232fcceb4a8ff9bfda958c991765c879240c17
mario muuk <62722269+DrSuperMario@users.noreply.github.com>
2021-01-15 15:06:43 +0200

    comitted

--- brain/knoweledge.py
+++ brain/knoweledge.py
@@ -6,11 +6,13 @@ pic = imgRead.imread('brain\ink.png')
 
 def tabel_of_know():
 
-    df = pd.DataFrame(index=None, columns=['Languages','Tools','Cloud','Enviroments', 'Other'])
+    df = pd.DataFrame(index=None, columns=['Languages','Tools','Cloud','Enviroments','Frameworks','Setup', 'Other'])
     df['Languages'] = ['JAVA','Python','SQL/HTML/CSS','Octave','Nan','Nan']
     df['Tools'] = ['VSCode','Intellij','Android Studio','Postman','Octave','PowerShell']
     df['Cloud'] = ['DigitalOcean','Heroku','TravisCI','Nan','Nan','Nan']
     df['Enviroments'] = ['Windows', 'Ubuntu', 'ArchLinux','Nan','Nan','Nan']
+    df['Frameworks'] = ['Flask','AndroidAPI','TensorFlow','NLTK','Nan','Nan']
+    df['Setup'] = ['HP Pavilion 15','Raspberry pi4B','Raspberry pi400','RaspberryZero','Nan','Nan']
     df['Other'] = ['Machine Learning','Deep Learning','Data Science','Nan','Nan','Nan']
     df = df.replace('Nan',' ')
     return df.style.set_properties(**{'background-color': 'white',
